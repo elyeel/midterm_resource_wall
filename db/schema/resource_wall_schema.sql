42457eb945edaf7a8b8ee0dba4da1e16351c662d
elyeel <elyeel@yahoo.com>
2019-08-26 20:21:12 +0000

    modified schema

--- db/schema/resource_wall_schema.sql
+++ db/schema/resource_wall_schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE users (
   user_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  avatar VARCHAR(255),
+  avatar_url VARCHAR(255),
   date_join DATE
 );
 
