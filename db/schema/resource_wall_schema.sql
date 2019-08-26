3238ba6b349f80f2c2d6da238d39548b23a08afa
elyeel <elyeel@yahoo.com>
2019-08-26 19:45:06 +0000

    added database schema, like, rating, comment seeds

---
+++ db/schema/resource_wall_schema.sql
@@ -0,0 +1,55 @@
+-- Drop and recreate Users table (Example)
+-- \c midterm;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS likes CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255),
+  date_join DATE
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE ,
+  description VARCHAR(255) NOT NULL,
+  resource_img VARCHAR(255)
+);
+
+CREATE TABLE likes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  val SMALLINT
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  comment VARCHAR(255),
+  created_at DATE
+);
+
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
+  rating SMALLINT
+);
---
+++ db/seeds/02_likes_rating_comment.sql
@@ -0,0 +1,32 @@
+-- likes, ratings, comments table seeds here (Example)
+
+INSERT INTO likes (user_id, resource_id) VALUES
+(1, 1, 1),
+(1, 4, 0),
+(2, 2, 0),
+(2, 3, 1),
+(3, 4, 1),
+(3, 5, 1),
+(4, 7, 1),
+(4, 3, 0),
+(5, 6, 0),
+(5, 9, 1);
+
+INSERT INTO comments (user_id, resource_id, comment, created_at)
+VALUES
+(1, 5, 'This is a nice source for Photography! Love it!', '2019-04-15' ),
+(2, 8, 'Supercool features, hope I can make it as my DIY project.','2019-01-25'),
+(3, 3, 'Ahh, this make sense to me better than my school books', '2019-05-08'),
+(4, 7, 'Too complicated, need something simpler and efficient', '2019-03-30'),
+(5, 8, 'a bit too advanced for me', '2019-06-21'),
+(2, 8, 'try to find local resources for parts', '2019-05-11'),
+(6, 1, 'I should not check this, wasting my time', '2019-06-29');
+
+INSERT INTO ratings (user_id, resource_id, rating) VALUES
+(4, 2, 4),
+(1, 3, 1),
+(1, 4, 3),
+(3, 9, 5),
+(2, 8, 3),
+(5, 6, 2),
+(5, 4, 4);
